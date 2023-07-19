edddd9fbda0887305a9561d882a143e92ae3bcd2
Sanket Kachare <102133582+Sanketsk74@users.noreply.github.com>
2023-07-19 17:00:21 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,32 @@
+let songs = [
+    {
+        name: 'Broken Angel',
+        path: 'musics/Broken_Angel.mp3',
+        artist: 'Arash',
+        cover: 'images/cover 1.png'
+    },
+    {
+        name: 'Faded',
+        path: 'musics/Faded.mp3',
+        artist: 'Alan Walker',
+        cover: 'images/cover 2.png'
+    },
+    {
+        name: 'Albatraoz',
+        path: 'musics/Im_an_Albatraoz.mp3',
+        artist: 'Aron Chupa',
+        cover: 'images/cover 3.png'
+    },
+    {
+        name: 'Lonely',
+        path: 'musics/Lonely.mp3',
+        artist: 'Justin Bieber & Benny Blanco',
+        cover: 'images/cover 4.png'
+    },
+    {
+        name: 'Love',
+        path: 'musics/I_Need_Your_Love.mp3',
+        artist: 'Nightcore',
+        cover: 'images/cover 5.png'
+    },
+]
